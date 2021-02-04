3e93ddf25208faf99141546a24a527dd6e7d32b5
Harris <77555458+harris-turtle@users.noreply.github.com>
2021-02-04 00:41:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from vpython import *
+
+G=6.67e-11 
+unitMass=7.348e22
+
+rem=384.4e6
+
+mA=sphere(pos=vector(0,0,0), radius=rem, color=color.yellow, make_trail=False)
+mB=sphere(pos=vector(15*rem, 0, 0), radius=rem/3, color=color.green, make_trail=False)
+mC=sphere(pos=vector(15.5*rem, 0,0), radius=rem/5, color=color.cyan, make_trail=False)
+
+mA.m=6000*unitMass
+mB.m=100*unitMass
+mC.m=0.055*unitMass
+
+mA.p=mA.m*vector(0,0,0)
+mB.p=mB.m*vector(0,-2000,0)
+mC.p=mC.m*vector(0,-180,0)
+
+t=0
+dt=3600
+
+tmax=24*dt*28
+
+while t<tmax*10:
+  rate(500)
+  rAB=mB.pos-mA.pos
+  rAC=mC.pos-mA.pos
+  rBC=mC.pos-mB.pos
+  
+  FAB=-G*mA.m*mB.m*norm(rAB)/mag(rAB)**2
+  FAC=-G*mA.m*mC.m*norm(rAC)/mag(rAC)**2
+  FBC=-G*mB.m*mC.m*norm(rBC)/mag(rBC)**2
+  
+  mA.p=mA.p+(-FAB-FAC)*dt
+  mB.p=mB.p+(FAB-FBC)*dt
+  mC.p=mC.p+(FAC+FBC)*dt
+  
+  mA.pos=mA.pos+mA.p*dt/mA.m
+  mB.pos=mB.pos+mB.p*dt/mB.m
+  mC.pos=mC.pos+mC.p*dt/mC.m
+  
+  t=t+dt
