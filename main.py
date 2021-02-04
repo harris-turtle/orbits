227ee2161684d2a5c4e350630261322c8a4f93e0
Harris <77555458+harris-turtle@users.noreply.github.com>
2021-02-04 01:11:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from vpython import *
 
+#Initial values
 G=6.67e-11 
 unitMass=7.348e22
 
 rem=384.4e6
 
+#Drawing bodies
 mA=sphere(pos=vector(0,0,0), radius=rem, color=color.yellow, make_trail=False)
 mB=sphere(pos=vector(15*rem, 0, 0), radius=rem/3, color=color.green, make_trail=False)
 mC=sphere(pos=vector(15.5*rem, 0,0), radius=rem/5, color=color.cyan, make_trail=False)
@@ -17,11 +19,15 @@ mA.p=mA.m*vector(0,0,0)
 mB.p=mB.m*vector(0,-2000,0)
 mC.p=mC.m*vector(0,-180,0)
 
+#Creating center of mass
+com=sphere(pos=(mB.m*mB.pos + mA.m*mA.pos)/(mB.m+mA.m), radius=rem/4, color=color.red, make_trail=True)
+
+#Time steps scaled to be appropriate
 t=0
 dt=3600
-
 tmax=24*dt*28
 
+#Updating position values
 while t<tmax*10:
   rate(500)
   rAB=mB.pos-mA.pos
@@ -39,5 +45,7 @@ while t<tmax*10:
   mA.pos=mA.pos+mA.p*dt/mA.m
   mB.pos=mB.pos+mB.p*dt/mB.m
   mC.pos=mC.pos+mC.p*dt/mC.m
+  
+  com.pos=(mB.m*mB.pos + mA.m*mA.pos)/(mB.m+mA.m)
   
   t=t+dt
